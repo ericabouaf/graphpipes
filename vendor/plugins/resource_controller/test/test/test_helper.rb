45977b2610c7c5fca1cb20e8d7d71b6ef76a4b2c
Thomas Winkler <tom@formrausch.com>
2008-02-05 13:50:51 +0100

    Added resource_controller plugin

---
+++ vendor/plugins/resource_controller/test/test/test_helper.rb
@@ -0,0 +1,11 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'test_help'
+require 'mocha'
+
+class Test::Unit::TestCase
+  self.use_transactional_fixtures = true
+  self.use_instantiated_fixtures  = false
+  
+  load_all_fixtures
+end
