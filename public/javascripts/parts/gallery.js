39185aac59b3695cec628a5b95fcf2b0cc10030c
Thomas Winkler <tom@formrausch.com>
2008-02-06 23:10:18 +0100

    Rewrite of Gallery: Gallery class

--- public/javascripts/parts/gallery.js
+++ public/javascripts/parts/gallery.js
@@ -1,23 +1,64 @@
-var move_to_step = function(id) {
-  var width = 975
-  var position = id * width - width
-  new Effect.Move ('all',{ x: -position, y: 0, mode: 'absolute'}); 
-  return false;
-}
+Gallery = Class.create({
+  
+  tabs : [],
+  
+  initialize : function(){
+    var me = this
+    console.log('init')
+    
+    this.tabs = this.find_gallery_tabs()
+    this.activate_tab(this.tabs[0])
+    
+    this.tabs.each(function(tab) {
+      tab.href = '#' + tab.innerHTML.gsub(/[^a-z1-9]+/i, '-')
+      tab.observe('click', function() {
+        me.move_to_step(me.tabs.indexOf(tab) + 1)
+      })
+    })
+  },
+  
+  move_to_step : function(id) {
+    this.activate_tab(this.tabs[id - 1])    
+
+    var width = 975
+    var position = id * width - width
+    new Effect.Move ('all',{ x: -position, y: 0, mode: 'absolute'}); 
+    return false;
+  },
+  
+  activate_tab : function(tab) {
+    this.tabs.each(function(b) { b.removeClassName('current') })
+    tab.addClassName('current')
+  },
+  
+  find_gallery_tabs : function() {
+    return $$('ul.gallery_tabs li a')
+  }
+  
+})
+
+// var move_to_step = function(id) {
+//   var tabs = find_gallery_tabs()
+//   activate_tab(tabs[id - 1])    
+//   
+//   var width = 975
+//   var position = id * width - width
+//   new Effect.Move ('all',{ x: -position, y: 0, mode: 'absolute'}); 
+//   return false;
+// }
+
+// var activate_tab = function(tab) {
+//   var tabs = find_gallery_tabs()
+//   tabs.each(function(b) { b.removeClassName('current') })
+//   tab.addClassName('current')
+// }
+
+// var find_gallery_tabs = function() {
+//   return $$('ul.gallery_tabs li a')
+// }
 
 Event.observe(window, 'dom:loaded', function() { 
-  var tabs = $$('ul.gallery_tabs li a')
-  
-  tabs[0].addClassName('current')
-  
-  tabs.each(function(tab) {
-    tab.href = '#' + tab.innerHTML.gsub(/[^a-z1-9]+/i, '-')
-    tab.observe('click', function() {
-      tabs.each(function(b) { b.removeClassName('current') })
-      this.addClassName('current')      
-      move_to_step(tabs.indexOf(tab) + 1)
-    })
-  })  
+  header_gallery = new Gallery()
 })
 
 
