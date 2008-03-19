1866b492cfa8a075fe723b0e82c1e95f4295b95d
tomfarm <tom@formrausch.com>
2008-03-19 10:14:14 +0100

    removed signup in public version

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,19 +44,19 @@ class UsersController < ApplicationController
      end
    end
   
-  def create
-    cookies.delete :auth_token
-
-    @user = User.new(params[:user])
-    @user.save
-    if @user.errors.empty?
-      self.current_user = @user
-      redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!"
-    else
-      render :action => 'new', :layout => 'login'
-    end
-  end
+  # def create
+  #     cookies.delete :auth_token
+  # 
+  #     @user = User.new(params[:user])
+  #     @user.save
+  #     if @user.errors.empty?
+  #       self.current_user = @user
+  #       redirect_back_or_default('/')
+  #       flash[:notice] = "Thanks for signing up!"
+  #     else
+  #       render :action => 'new', :layout => 'login'
+  #     end
+  #   end
 
   def activate
     self.current_user = params[:activation_code].blank? ? :false : User.find_by_activation_code(params[:activation_code])
