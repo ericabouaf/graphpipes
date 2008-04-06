679f3532bcf0ae6e7d361d1e719d148b45cbb4e5
tomfarm <tom@formrausch.com>
2008-04-06 10:41:04 +0200

    testing subclassing jsBox for different boxes. boxes can now be created (patch) by setting {id:box.id} as second parameter

--- public/javascripts/patch.js
+++ public/javascripts/patch.js
@@ -1,27 +1,34 @@
 var Patch = Class.create({
   initialize: function(element, options) {
-    var kind = element.innerHTML.toLowerCase()
+    console.log(options.id)
+    var kind = options.id || element.innerHTML.toLowerCase()
     this.run(kind)
   },
   run: function(func_name) {
     var operation = this["new_" + func_name]
+    console.log("new_" + func_name)
     if (this["new_" + func_name] != undefined) { 
       this["new_" + func_name](func_name)
     } else {
       error('Operation: ' + func_name + ': Not implemented yet')
     }
   },
-
+  
+ new_nav_rdf_resource: function(as) {
+   this.build_me('repositoryBox')
+ },
+  
   new_regex: function(as) {
-    this.build_me(as)  
+    this.build_me('jsBox')  
   },
   
   new_union: function(as) {
-    this.build_me(as)  
+    this.build_me('jsBox')  
   },
   
   build_me: function(namespace) {
-    window.jsBox.addModule();
+    var name = namespace 
+    window[name].addModule();
   }
 })
 
